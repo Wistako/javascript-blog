c606b6a72f51cc206337a5664a7904bf6c759fca
Piotr Słomczyński <wistako@gmail.com>
2023-10-12 14:56:42 +0200

    Add Tags

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 {
   'use strict';
+  const optArticleTagsSelector = '.post-tags .list';
   /*
 document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
@@ -11,7 +12,7 @@ document.getElementById('test-button').addEventListener('click', function(){
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
-  
+
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -22,7 +23,7 @@ document.getElementById('test-button').addEventListener('click', function(){
 
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
-  
+
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
     for(let activeArticle of activeArticles){
@@ -49,7 +50,7 @@ document.getElementById('test-button').addEventListener('click', function(){
     const articles = document.querySelectorAll('.post');
     let linkHTML = '';
     for( let article of articles){
-        
+
       /* get the article id */
       const articleId = article.getAttribute('id');
       console.log(articleId);
@@ -65,9 +66,8 @@ document.getElementById('test-button').addEventListener('click', function(){
       linkHTML +='<li><a href="#' + articleId + '"><span>' + articleTitle  + '</span></a></li>';
     }
     /* insert link into titleList */
-    
+
     document.querySelector('.titles').innerHTML = linkHTML;
-    console.log(linkHTML);
     const links = document.querySelectorAll('.titles a');
     for (let link of links){
       link.addEventListener('click', titleClickHandler);
@@ -75,4 +75,34 @@ document.getElementById('test-button').addEventListener('click', function(){
   };
 
   generateTitleLinks();
+
+  const generateTags =function(){
+    /* find all articles */
+    const articles = document.querySelectorAll('.post');
+    /* START LOOP: for every article: */
+    for(let article of articles){
+      /* find tags wrapper */
+      const tagsWrapper = article.querySelector(optArticleTagsSelector);
+      /* make html variable with empty string */
+      let taglinkHTML = '';
+      /* get tags from data-tags attribute */
+      const tags = article.getAttribute('data-tags');
+      /* split tags into array */
+      const tagsAray = tags.split(' ');
+      /* START LOOP: for each tag */
+      for(let genTag of tagsAray){
+        /* generate HTML of the link */
+        const tag = '<li><a href="#">' + genTag + '</a></li>';
+        /* add generated code to html variable */
+        taglinkHTML += tag + ' ';
+        console.log(taglinkHTML);
+      /* END LOOP: for each tag */
+      }
+      /* insert HTML of all the links into the tags wrapper */
+      tagsWrapper.innerHTML = taglinkHTML;
+    /* END LOOP: for every article: */
+    }
+  };
+
+  generateTags();
 }
